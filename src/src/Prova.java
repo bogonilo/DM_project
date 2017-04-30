a514ef5365e849eef2501f3b0fd2e7b1c4728dd2
dalde <dalde@hotmail.it>
2017-05-01 00:34:25 +0200

    1

--- src/src/Prova.java
+++ src/src/Prova.java
@@ -4,9 +4,10 @@
 public class Prova {
 
     public static void main(String [] args){
-        System.out.print("HEEEEELLLLLOOOOOO");
-        System.out.print("HEEEEELLLLLOOOOOO");
-        System.out.print("funziona can dal porco?");
+        System.out.println("HEEEEELLLLLOOOOOO");
+        System.out.println("HEEEEELLLLLOOOOOO");
+        System.out.println("funziona can dal porco?");
+        System.out.println("Funziona tutto grazie al cielo. by DDD");
     }
 
 }
